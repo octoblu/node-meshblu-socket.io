2d7fc406587c9ec6363ab270f248cd803b663f8c
Chris Matthieu <chris@matthieu.us>
2013-10-08 14:03:08 -0700

    added connect

---
+++ README.md
@@ -0,0 +1,19 @@
+```
+ SSSSS  kk                            tt    
+SS      kk  kk yy   yy nn nnn    eee  tt    
+ SSSSS  kkkkk  yy   yy nnn  nn ee   e tttt  
+     SS kk kk   yyyyyy nn   nn eeeee  tt    
+ SSSSS  kk  kk      yy nn   nn  eeeee  tttt 
+                yyyyy                         
+```
+======
+
+Phase 1 - Build a network and realtime API for enabling machine-to-machine communications.
+
+Here are several quick screencasts that demostrate what you can do with Skynet:
+
+[POC Video 1](http://www.youtube.com/watch?v=cPs1JNFyXjk)
+
+[POC Video 2](http://www.youtube.com/watch?v=SzaTPiaDDQI)
+
+[POC Video 3](http://www.youtube.com/watch?v=TB6RyzT10EA)
--- index.js
+++ index.js
@@ -1,3 +1,57 @@
-module.exports = function(num) {
-  console.log(num, 'skynets');
+var io = require("socket.io-client");
+
+exports.connect = function(connectionParams){
+// module.exports = function connect(connectionParams) {
+
+  var host = connectionParams.host
+  var port = connectionParams.port
+  var uuid = connectionParams.uuid
+  var token = connectionParams.token
+
+  // Websocket controls
+  this.socket = io.connect(host, {
+      port: port
+  });
+
+  this.socket.on('connect', function(){
+    console.log('Requesting websocket connection to Skynet');
+
+    socket.on('identify', function(data){
+      console.log('Websocket connected to Skynet with socket id: ' + data.socketid);
+      console.log("Sending device uuid: " + uuid);
+      socket.emit('identity', {uuid: uuid, socketid: data.socketid, token: token});
+    });
+
+    socket.on('authentication', function(data){
+      if (data.status == 201){
+        console.log('Device authenticated with Skynet');
+      } else { // 401
+        console.log('Device not authenticated with Skynet');
+      }
+    });
+
+    // test APIs
+    this.socket.emit('status');
+    // socket.on('status', function(data){
+    //   console.log('status received');
+    //   console.log(data);
+    // });
+
+
+    // socket.emit('update', {"uuid":"ad698900-2546-11e3-87fb-c560cb0ca47b", "token": "zh4p7as90pt1q0k98fzvwmc9rmjkyb9", "key":"777"});
+    socket.on('update', function(data){
+      console.log('update received');
+      console.log(data);
+    });
+
+
+    socket.on('message', function(data){
+      console.log(data);      
+    });
+    socket.on('disconnect', function(){
+      console.log('disconnect');
+    });
+
+  });
+
 };
